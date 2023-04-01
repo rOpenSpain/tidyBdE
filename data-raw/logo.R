bb294ad0bce552a7834a5dcc210487de4d04bae0
Diego Hernangómez Herrero <diego.hernangomezherrero@gmail.com>
2023-04-01 10:01:11 +0200

    Change logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -3,39 +3,18 @@ library(hexSticker)
 library(showtext)
 library(tidyBdE)
 
-# IE <- bde_catalog_search("(.*)PIB(.*)Tasa interanual", catalog = "IE")
-
-PIB_ESP <-
-  bde_series_full_load("BE0201.csv") %>%
-  select(c("Date", "BE_2_1.21"))
-
-PIB_selected <- bde_series_full_load("IE0201.csv") %>%
-  select(c("Date", paste0("IE_2_1.", 5:7)))
-
-
-
-bde_col_names <- unique(c(names(PIB_ESP), names(PIB_selected)))
-
-PIB_merge <- PIB_ESP %>%
-  inner_join(PIB_selected) %>%
+plotseries <- bde_ind_gdp_var("GDP YoY", out_format = "long") %>%
+  bind_rows(
+    bde_ind_unemployment_rate("Unemployment Rate", out_format = "long")
+  ) %>%
   drop_na() %>%
-  pivot_longer(cols = bde_col_names[bde_col_names != "Date"]) %>%
-  filter(Date > "2000-01-01" & Date <= "2018-12-31") %>%
-  mutate(PIB_perc = value)
+  filter(Date >= "2010-01-01")
 
-
-g <- ggplot(PIB_merge, aes(x = Date, y = PIB_perc)) +
-  geom_hline(yintercept = 0, colour = "black") +
-  geom_line(aes(color = name, linetype = name), size = 0.5) +
-  scale_linetype_manual(values = c(rep("solid", 3), "dashed")) +
-  bde_scale_color_vivid() +
-  theme_tidybde() +
-  theme(
-    legend.position = "none",
-    line = element_line(size = 0.2)
-  ) +
-  theme_transparent()
-
+g <- ggplot(plotseries, aes(x = Date, y = serie_value)) +
+  geom_line(aes(color = serie_name), linewidth = 0.25, show.legend = FALSE) +
+  theme_minimal() +
+  labs(x="", y="") +
+  scale_color_bde_d(palette = "bde_vivid_pal") # Custom palette on the package
 
 # font_add_google("Roboto", "roboto")
 showtext_auto()
@@ -52,9 +31,11 @@ sticker(
   p_color = "#993300",
   h_fill = "grey95",
   h_color = "#993300",
-  s_width = 1.83,
+  s_width = 1.6,
   s_height = 1,
   p_size = 30,
-  s_x = 1.01,
-  s_y = 0.75,
+  s_x = .95,
+  s_y = 0.7,
 )
+
+pkgdown::build_favicons(overwrite = TRUE)
