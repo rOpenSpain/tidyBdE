7e31d99c9db326eb7083cefb15dce848d6b30cdd
Diego H. <diego.hernangomezherrero@gmail.com>
2026-06-22 09:22:11 +0200

    Safe tests with on.exit

--- tests/testthat/test-bde-check-access.R
+++ tests/testthat/test-bde-check-access.R
@@ -2,19 +2,26 @@ test_that("On CRAN", {
   skip_on_cran()
   skip_if_bde_offline()
 
+  env_orig <- Sys.getenv("NOT_CRAN", unset = NA_character_)
+
+  on.exit(
+    {
+      if (is.na(env_orig)) {
+        Sys.unsetenv("NOT_CRAN")
+      } else {
+        Sys.setenv("NOT_CRAN" = env_orig)
+      }
+    },
+    add = TRUE
+  )
+
   # Imagine we are in CRAN
-  env_orig <- Sys.getenv("NOT_CRAN")
   Sys.setenv("NOT_CRAN" = "false")
   expect_true(on_cran())
   expect_false(bde_check_access())
 
   Sys.setenv("NOT_CRAN" = "")
   expect_identical(!interactive(), on_cran())
-
-  # Restore
-  Sys.setenv("NOT_CRAN" = env_orig)
-  expect_identical(Sys.getenv("NOT_CRAN"), env_orig)
-  expect_true(bde_check_access())
 })
 
 test_that("Check url access", {
